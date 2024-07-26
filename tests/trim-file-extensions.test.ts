956eed80444a2fe37bf6a5d604900a8b60eb6a67
Felix Eklöf <felixeklof@icloud.com>
2024-07-26 13:08:30 +0200

    test: add test for trim index fn

--- tests/trim-file-extensions.test.ts
+++ tests/trim-file-extensions.test.ts
@@ -1,14 +1,11 @@
 import { describe, expect, it } from "vitest";
-import { trimFileExtensions } from "../src/index";
+import { trimFileExtensions } from "../src";
 
 describe("trimFileExtensions", () => {
   it("removes file extensions", () => {
-    expect(trimFileExtensions([
-      "index.astro",
-      "file.route.astro",
-    ])).toEqual([
+    expect(trimFileExtensions(["index.astro", "file.route.astro"])).toEqual([
       "index",
-      "file.route"
+      "file.route",
     ]);
   });
 });
---
+++ tests/trim-index.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { trimIndex } from "../src";
+
+describe("trimIndex", () => {
+  it("removes text index from end of string", () => {
+    expect(trimIndex(["posts/index", "posts/[id]"])).toEqual([
+      "posts/",
+      "posts/[id]",
+    ]);
+  });
+});
