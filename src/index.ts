33922fce81c4f71e1a03bc012d2c541a64c538bf
Felix Eklöf <felixeklof@icloud.com>
2024-09-17 14:18:32 +0200

    Revert "Fix/fastglob not available in browser (#15)"
    This reverts commit dc1746faa1c2d06cec63d83b995fc8cca90455a0.

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ export async function getRouteFileContent(routes: DynamicRoute[]) {
   const routesObject = Object.fromEntries(routeEntries);
 
   return `
-  declare module "astro-typesafe-routes/path" {
+  declare module "astro-typesafe-routes" {
     export type Routes = ${JSON.stringify(routesObject)};
   
     type Route = keyof Routes;
@@ -114,6 +114,46 @@ export async function runCodeGen(options: RunCodeGenOptions) {
   const formattedContent = await formatPrettier(fileContent);
   await writeRouteFile(options.outputPath, formattedContent);
   return routes;
+}
+
+export type PathParameters = [
+  pathName: string,
+  options?: {
+    params?: Record<string, string>;
+    trailingSlash?: boolean;
+    search?: ConstructorParameters<typeof URLSearchParams>[0];
+    hash?: string;
+  },
+];
+
+export function $path(...args: PathParameters) {
+  const [pathName, options] = args;
+  const trailingSlash = options?.trailingSlash ?? false;
+
+  let url: string = pathName;
+
+  if (trailingSlash) {
+    url += "/";
+  }
+
+  if (options?.params !== undefined) {
+    for (const [paramKey, paramValue] of Object.entries<string>(
+      options.params,
+    )) {
+      url = url.replace(`[${paramKey}]`, paramValue);
+    }
+  }
+
+  const search = new URLSearchParams(options?.search);
+  if (search.size > 0) {
+    url += `?${search.toString()}`;
+  }
+
+  if (options?.hash !== undefined) {
+    url += `#${options.hash}`;
+  }
+
+  return url;
 }
 
 export function logSuccess(logger: AstroIntegrationLogger, outputPath: string) {
