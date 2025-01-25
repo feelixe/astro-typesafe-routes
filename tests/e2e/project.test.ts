91291b1af8bee655f0cced93b9f1c6af38c9272b
Felix Eklöf <felixeklof@icloud.com>
2025-01-25 22:08:02 +0100

    fix: copy with fs

--- tests/e2e/project.test.ts
+++ tests/e2e/project.test.ts
@@ -1,39 +1,45 @@
 import { describe, beforeAll, it, afterAll, expect } from "bun:test";
 import { $ } from "bun";
+import { cp } from "node:fs/promises";
+import path from "node:path";
 
 const rootDir = import.meta.dir;
 
 describe("setup", async () => {
   beforeAll(async () => {
     // Copy Astro template project
-    await $`cd ${rootDir} && cp -r ./project-template ./project`;
+    const templateDir = path.join(rootDir, "project-template");
+    const projectDir = path.join(rootDir, "project");
+    await cp(templateDir, projectDir, { recursive: true });
 
     // Install test-project dependencies
-    await $`cd ${rootDir} && cd ./project && bun install`;
+    await $`cd ./project && bun install`.cwd(rootDir);
 
     // Build package
-    await $`cd ${rootDir} && cd ../../ && bun run build`;
+    await $`cd ../../ && bun run build`.cwd(rootDir);
 
     // Add bun link to package
-    await $`cd ${rootDir} && cd ../../ && pwd && bun link`;
+    await $`cd ../../ && pwd && bun link`.cwd(rootDir);
 
     // Install bun linked package
-    await $`cd ${rootDir} && cd ./project && bun link astro-typesafe-routes --save`;
+    await $`cd ./project && bun link astro-typesafe-routes --save`.cwd(rootDir);
   });
 
   it("build fails when project contains an invalid link", async () => {
     await expect(async () => {
-      await $`cd ${rootDir} && cd ./project && bun run build`;
+      await $`cd ./project && bun run build`.cwd(rootDir);
     }).toThrow();
   }, 20_000);
 
   it("build succeeds when project contains only valid links", async () => {
-    await $`cd ${rootDir} && cd ./project && rm ./src/pages/page-with-invalid-link.astro`;
+    await $`cd ./project && rm ./src/pages/page-with-invalid-link.astro`.cwd(
+      rootDir,
+    );
 
-    await $`cd ${rootDir} && cd ./project && bun run build`;
+    await $`cd ./project && bun run build`.cwd(rootDir);
   }, 20_000);
 
   afterAll(async () => {
-    await $`cd ${rootDir} && rm -rf ./project`;
+    await $`rm -rf ./project`.cwd(rootDir);
   });
 });
