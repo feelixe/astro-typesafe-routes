801a1fd82222d27d3f17cb02e330342e268ad865
Felix Eklöf <felixeklof@icloud.com>
2025-01-26 15:53:52 +0100

    fix: change test project our dir

--- tests/e2e/project.test.ts
+++ tests/e2e/project.test.ts
@@ -40,20 +40,21 @@ const setups = [
   {
     name: "Astro v4",
     templateDir: path.join(rootDir, "./project-templates/astro-v4"),
+    outDir: path.join(rootDir, "./project-astro-v4"),
   },
   {
     name: "Astro v5",
     templateDir: path.join(rootDir, "./project-templates/astro-v5"),
+    outDir: path.join(rootDir, "./project-astro-v5"),
   },
 ];
 
 describe.each(setups)("$name", (args) => {
-  const projectDir = path.join(rootDir, "project");
   const packageDir = path.join(rootDir, "../../");
 
   beforeEach(async () => {
     await setupTestProject({
-      outDir: projectDir,
+      outDir: args.outDir,
       templateDir: args.templateDir,
       packageDir,
     });
@@ -61,23 +62,23 @@ describe.each(setups)("$name", (args) => {
 
   afterEach(async () => {
     await cleanUpTestProject({
-      dir: projectDir,
+      dir: args.outDir,
     });
   });
 
   it("build fails when project contains an invalid link", async () => {
     await expect(async () => {
-      await $`bun run build`.cwd(projectDir);
+      await $`bun run build`.cwd(args.outDir);
     }).toThrow();
   }, 20_000);
 
   it("build succeeds when project contains only valid links", async () => {
     const invalidPage = path.join(
-      projectDir,
+      args.outDir,
       "./src/pages/page-with-invalid-link.astro",
     );
     await rm(invalidPage);
 
-    await $`bun run build`.cwd(projectDir);
+    await $`bun run build`.cwd(args.outDir);
   }, 20_000);
 });
