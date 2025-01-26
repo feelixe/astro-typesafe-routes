5b8f054957ba0a123b59a27a56b4aface1c40a29
Felix Eklöf <felixeklof@icloud.com>
2025-01-26 16:48:41 +0100

    test: remove broken dir deletion

--- tests/e2e/project.test.ts
+++ tests/e2e/project.test.ts
@@ -2,56 +2,25 @@ import { describe, it, expect, beforeEach, afterEach } from "bun:test";
 import { $ } from "bun";
 import { cp, rm } from "node:fs/promises";
 import path from "node:path";
+import { cleanUpTestProject, setupTestProject } from "./project-utils";
 
 const rootDir = import.meta.dir;
-
-type SetupTestProjectArgs = {
-  templateDir: string;
-  outDir: string;
-  packageDir: string;
-};
-
-async function setupTestProject(args: SetupTestProjectArgs) {
-  // Copy template
-  await cp(args.templateDir, args.outDir, { recursive: true });
-
-  // Install test-project dependencies
-  await $`bun install`.cwd(args.outDir);
-
-  // Build package
-  await $`bun run build`.cwd(args.packageDir);
-
-  // Add bun link to package
-  await $`bun link`.cwd(args.packageDir);
-
-  // Install bun linked package
-  await $`bun link astro-typesafe-routes --save`.cwd(args.outDir);
-}
-
-type CleanUpTestProjectArgs = {
-  dir: string;
-};
-
-async function cleanUpTestProject(args: CleanUpTestProjectArgs) {
-  await $`rm -rf ${args.dir}`;
-}
+const packageDir = path.join(rootDir, "../../");
 
 const setups = [
   {
     name: "Astro v4",
     templateDir: path.join(rootDir, "./project-templates/astro-v4"),
-    outDir: path.join(rootDir, "./project-astro-v4"),
+    outDir: path.join(rootDir, "./projects/project-astro-v4"),
   },
   {
     name: "Astro v5",
     templateDir: path.join(rootDir, "./project-templates/astro-v5"),
-    outDir: path.join(rootDir, "./project-astro-v5"),
+    outDir: path.join(rootDir, "./projects/project-astro-v5"),
   },
 ];
 
 describe.each(setups)("$name", (args) => {
-  const packageDir = path.join(rootDir, "../../");
-
   beforeEach(async () => {
     await setupTestProject({
       outDir: args.outDir,
