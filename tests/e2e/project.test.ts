34b5395f627962179c3d5b3c392eddd8b9718272
Felix Eklöf <felixeklof@icloud.com>
2025-02-01 21:13:40 +0100

    test: add project test templates

--- tests/e2e/project.test.ts
+++ tests/e2e/project.test.ts
@@ -10,11 +10,37 @@ const setups = [
   {
     name: "Astro v4 - valid path",
     templateDir: path.join(rootDir, "./project-templates/v4-valid-path"),
-    expectBuildSuccess: false,
+    expectBuildSuccess: true,
   },
   {
     name: "Astro v4 - invalid path",
     templateDir: path.join(rootDir, "./project-templates/v4-invalid-path"),
+    expectBuildSuccess: false,
+  },
+  {
+    name: "Astro v5 - valid path",
+    templateDir: path.join(rootDir, "./project-templates/v5-valid-path"),
+    expectBuildSuccess: true,
+  },
+  {
+    name: "Astro v5 - invalid path",
+    templateDir: path.join(rootDir, "./project-templates/v5-invalid-path"),
+    expectBuildSuccess: false,
+  },
+  {
+    name: "Astro v5 - valid search params",
+    templateDir: path.join(
+      rootDir,
+      "./project-templates/v5-valid-search-params",
+    ),
+    expectBuildSuccess: true,
+  },
+  {
+    name: "Astro v5 - invalid search params",
+    templateDir: path.join(
+      rootDir,
+      "./project-templates/v5-invalid-search-params",
+    ),
     expectBuildSuccess: false,
   },
 ];
@@ -22,14 +48,15 @@ const setups = [
 for (const setup of setups) {
   describe(setup.name, async () => {
     const outDir = setup.templateDir.split(path.sep).at(-1);
-
     if (!outDir) {
       throw new Error("Expected outDir to be defined");
     }
 
+    const absoluteOutDir = path.join(rootDir, "projects", outDir);
+
     await beforeAll(async () => {
       await setupTestProject({
-        outDir,
+        outDir: absoluteOutDir,
         templateDir: setup.templateDir,
         packageDir,
       });
@@ -37,18 +64,18 @@ for (const setup of setups) {
 
     await afterAll(async () => {
       await cleanUpTestProject({
-        dir: outDir,
+        dir: absoluteOutDir,
       });
     });
 
     if (setup.expectBuildSuccess) {
       it("build without errors when project contains only valid links", async () => {
-        await $`bun run build`.cwd(outDir);
+        await $`bun run build`.cwd(absoluteOutDir);
       }, 20_000);
     } else {
       it("build fails when project contains an invalid link", async () => {
         await expect(async () => {
-          await $`bun run build`.cwd(outDir);
+          await $`bun run build`.cwd(absoluteOutDir);
         }).toThrow();
       }, 20_000);
     }
