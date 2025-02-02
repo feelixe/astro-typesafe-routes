9daddd82f18d7b04beb884d1dbf5e3f0b0bc25e5
Felix Eklöf <felixeklof@icloud.com>
2025-02-02 14:29:42 +0100

    test: add search param usage in example project

--- src/search.ts
+++ src/search.ts
@@ -2,17 +2,39 @@ import { AstroGlobal } from "astro";
 import { z } from "astro/zod";
 import { deserialize } from "./search-serializer.js";
 
-export function getSearch(astro: AstroGlobal, schema: z.ZodTypeAny) {
+export function getSearch<T extends z.ZodTypeAny>(
+  astro: AstroGlobal,
+  schema: T,
+) {
   const deserializedSearch = deserialize(astro.url.searchParams);
-  return schema.parse(deserializedSearch);
+  return schema.parse(deserializedSearch) as z.infer<T>;
 }
 
-export function getSearchSafe(astro: AstroGlobal, schema: z.ZodTypeAny) {
+export async function getSearchAsync<T extends z.ZodTypeAny>(
+  astro: AstroGlobal,
+  schema: T,
+) {
   const deserializedSearch = deserialize(astro.url.searchParams);
-  return schema.safeParse(deserializedSearch);
+  const parsedValue = await schema.parseAsync(deserializedSearch);
+  return parsedValue as z.infer<T>;
 }
 
-export function getSearchSafeAsync(astro: AstroGlobal, schema: z.ZodTypeAny) {
+export function getSearchSafe<T extends z.ZodTypeAny>(
+  astro: AstroGlobal,
+  schema: T,
+) {
   const deserializedSearch = deserialize(astro.url.searchParams);
-  return schema.safeParseAsync(deserializedSearch);
+  return schema.safeParse(deserializedSearch) as z.SafeParseReturnType<
+    z.input<T>,
+    z.output<T>
+  >;
+}
+
+export async function getSearchSafeAsync<T extends z.ZodTypeAny>(
+  astro: AstroGlobal,
+  schema: T,
+) {
+  const deserializedSearch = deserialize(astro.url.searchParams);
+  const validation = await schema.safeParseAsync(deserializedSearch);
+  return validation as z.SafeParseReturnType<z.input<T>, z.output<T>>;
 }
