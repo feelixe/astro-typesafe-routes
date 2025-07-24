9dc7532e450e7a1fe651b4f23af5c847b3abd828
Felix Eklöf <felixeklof@icloud.com>
2025-07-24 18:42:13 +0200

    feat: add react

--- apps/docs-v2/astro.config.mjs
+++ apps/docs-v2/astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
