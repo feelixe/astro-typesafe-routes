13550631bce465d6a4a9370295caefc025c48f80
Felix Eklöf <felixeklof@icloud.com>
2025-07-27 16:32:53 +0200

    chore: 5.0.0-rc13

--- e2e-projects/invalid-search-params/invalid-search-params.test.ts
+++ e2e-projects/invalid-search-params/invalid-search-params.test.ts
@@ -1,7 +1,7 @@
 import { beforeAll, describe } from "bun:test";
 import { expectBuildFailure, setupTestProject } from "../utils.ts";
 
-describe("e2e - valid search params", async () => {
+describe("e2e - invalid search params", async () => {
   beforeAll(async () => {
     await setupTestProject({
       projectDir: import.meta.dir,
