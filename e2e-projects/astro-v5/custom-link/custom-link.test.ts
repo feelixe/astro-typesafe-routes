d21847217d7130e9701e37f91fc0332ee00cde4f
Felix Eklöf <felixeklof@icloud.com>
2026-08-08 15:18:25 +0200

    test: assert astro version in e2e tests

--- e2e-projects/astro-v5/custom-link/custom-link.test.ts
+++ e2e-projects/astro-v5/custom-link/custom-link.test.ts
@@ -1,10 +1,14 @@
 import { beforeAll, describe } from "bun:test";
-import { expectBuildSuccess, setupTestProject } from "../../utils.ts";
+import { assertAstroVersion, expectBuildSuccess, setupTestProject } from "../../utils.ts";
 
 describe("e2e - astro v5 - custom link", async () => {
   beforeAll(async () => {
     await setupTestProject({
       projectDir: import.meta.dir,
+    });
+    await assertAstroVersion({
+      projectDir: import.meta.dir,
+      range: "^5.0.0",
     });
   });
 
